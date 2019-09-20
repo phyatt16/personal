9b3e66d896dc56ec05e13cc212316dd12439a84f
phil <phyatt16@gmail.com>
2019-09-20 09:20:33 -0600

    spot it maker works well. I need to add functions for saving cards and for determining the number of cards to make

--- spot_it_maker/spot_it_maker.py
+++ spot_it_maker/spot_it_maker.py
@@ -8,56 +8,54 @@ class SpotItMaker():
         self.source_dir = source_dir
         self.out_dir = out_dir
         self.files = os.listdir(source_dir)
-        self.num_pics = len(self.files)
-        if(self.num_pics>57):
-            self.num_pics = 57
-
-        self.card_height = 800
-        self.card_width = 800
-        self.blank_card = 255*np.ones((self.card_height,self.card_width,3), np.uint8)
-
-    def bgr_to_bgra(self,img):
-        b_channel, g_channel, r_channel = cv2.split(img)
-        alpha_channel = np.ones(b_channel.shape, dtype=b_channel.dtype)*0
-        img_BGRA = cv2.merge((b_channel, g_channel, r_channel, alpha_channel))
-        return img_BGRA
 
     def scale_image_to_have_max_dimension(self,img,maxdim):
         new_xsize = img.shape[0]*maxdim/float(np.max(img.shape))
         new_ysize = img.shape[1]*maxdim/float(np.max(img.shape))
-        return cv2.resize(img,(int(new_xsize),int(new_ysize)))
+        return cv2.resize(img,(int(new_ysize),int(new_xsize)))
 
-    def randomly_place_image(self,img,card):
-
+    def get_random_placed_image_corners(self,img,card):
         x_point = np.random.randint(0,card.shape[0]-img.shape[0])
         y_point = np.random.randint(0,card.shape[1]-img.shape[1])
 
-        ## TODO - CHECK FOR IMAGE COLLISION
+        return x_point,x_point+img.shape[0],y_point,y_point+img.shape[1]
 
-        card[x_point:x_point+img.shape[0],y_point:y_point+img.shape[1]] = img
-
-        return card
+    def randomly_rotate_image(self,img):
+        return cv2.rotate(img,np.random.randint(0,3))
 
     def create_one_card(self,images):
-
-        card = 0*np.ones((1000,1000,3),np.uint8)
-        for i in xrange(0,len(images)):
+        card_size = 300
+        img_size = 100
+        card = -1*np.ones((card_size,card_size,3),np.uint8)
+        i = 0
+        while i < len(images):
             img = cv2.imread(self.source_dir+self.files[images[i]])
+            img = self.randomly_rotate_image(img)            
+            img = self.scale_image_to_have_max_dimension(img,img_size+np.random.randint(-img_size*.5,img_size*.5))
+            a,b,c,d = self.get_random_placed_image_corners(img,card)
 
-            #Do something to get random variation in scaling
-            img = self.scale_image_to_have_max_dimension(img,250+np.random.randint(-150,150))
-            # img = self.randomly_rotate_image(img)
-            card = self.randomly_place_image(img,card)
-            #img is now the same size as card, but has -1 everywhere except the image
+            count = 0
+            fits = True
+            while np.max(card[a:b,c:d])>-1:
+                img = cv2.imread(self.source_dir+self.files[images[i]])
+                img = self.randomly_rotate_image(img)                
+                img = self.scale_image_to_have_max_dimension(img,img_size+np.random.randint(-img_size*.5,img_size*.5))                
+                a,b,c,d = self.get_random_placed_image_corners(img,card)
+                count += 1
 
-            # while(self.has_collision(img,card)):
-            #     img = self.scale_image_to_have_max_dimension(img,maxdim)
-            #     img = self.randomly_rotate_image(img)
-            #     img = self.randomly_place_image(img,card)
+                if(count==1000):
+                    i=-1
+                    fits = False
+                    card = -1*np.ones((card_size,card_size,3),np.uint8)
+                    break
 
-            # card = alpha*card + beta*img
-
+            i += 1                
+            if fits:
+                card[a:b,c:d] = img
+                
         # Make card a type uint8 array
+        card[card==-1]=255
+        card = np.array(card,np.uint8)
         return card
 
         
@@ -84,6 +82,14 @@ class SpotItMaker():
         self.cards = []
         for i in xrange(0,len(img_number_array)):
             self.cards.append(self.create_one_card(img_number_array[i]))
+            print "Made card: ",i
+
+    def preview_cards(self):
+        for i in xrange(0,len(self.cards)):
+            cv2.imshow('card'+str(i),self.cards[i])
+            cv2.waitKey(1)
+        cv2.waitKey(0)
+
 
 if __name__=='__main__':
     source_folder = './spot_it_pics/'
@@ -92,4 +98,6 @@ if __name__=='__main__':
     maker = SpotItMaker(source_folder,destination_folder)
 
     maker.create_cards()
+    maker.preview_cards()
+
     
